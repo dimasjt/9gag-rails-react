98401f98994d08c80302bc9366b2b8e4c4b722b9
Dimas J. Taniawan <dimas.taniawan@bocistudio.com>
2018-01-29 11:49:00 +0700

    gif and video post

--- app/javascript/packs/components/Post.js
+++ app/javascript/packs/components/Post.js
@@ -5,11 +5,20 @@ class Post extends React.PureComponent {
     const { post } = this.props
 
     return (
-      <div className="card" style={{ marginTop: 10, marginBottom: 10 }}>
-        <img className="card-img-top" src={post.media.image} alt={post.title} />
-        <div className="card-body">
+      <div className="card" style={{ marginTop: 10, marginBottom: 10, width: "60%" }}>
+        <div className="card-body" style={{ paddingBottom: 0 }}>
           <h5 className="card-title" dangerouslySetInnerHTML={{ __html: post.title }}></h5>
         </div>
+        {
+          post.video ? (
+            <video controls poster={post.media.poster} loop={true}>
+              <source src={post.media.mp4} type="video/mp4"></source>
+              <source src={post.media.webvm} type="video/webvm"></source>
+            </video>
+          ) : (
+              <img className="card-img-top" src={post.media.image} alt={post.title} />
+            )
+        }
       </div>
     )
   }
